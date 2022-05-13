1e30cd7dc271c7216acd68058b201c10882d40e9
Farhad Siraj <farhad.siraj@ibm.com>
2022-05-13 16:57:17 -0400

    Added assets and products api
    Author:    Farhad Siraj <farhadsiraj@gmail.com>
    
    Date:      Fri May 13 16:57:17 2022 -0400

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'Angular-Test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-Test');
+    expect(app.pageTitle).toEqual('Angular-Test');
   });
 
   it('should render title', () => {
