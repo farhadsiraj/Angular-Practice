3844ddc2dcc8cfbe67a58b3a0d2b6ee748417b6d
Farhad Siraj <farhad.siraj@ibm.com>
2022-05-13 17:30:17 -0400

    Created product-list component
    Author:    Farhad Siraj <farhadsiraj@gmail.com>
    
    Date:      Fri May 13 17:30:17 2022 -0400

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,11 @@ import { Component } from "@angular/core";
   selector: 'pm-root',
   template:`
   <div><h1>{{pageTitle}}</h1>
-    <div>My First Component </div>
+    <pm-products></pm-products>
   </div>
   `
 })
 
 export class AppComponent {
-  pageTitle: string = "Acne Product Management"
+  pageTitle: string = "Acme Product Management"
 }
